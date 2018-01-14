ad99e34b0ef9f61e8c49171dec4412bed56c26c9
iterevo1 <iterevo@gmail.com>
2018-01-14 13:24:59 -0600

    chenged gamedata to match data

---
+++ PowerUp/src/org/usfirst/frc/team967/lib/util/MatchData.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team967.lib.util;
+
+import edu.wpi.first.wpilibj.DriverStation;
+
+public class MatchData {
+	
+	public static enum GameFeature {
+		SWITCH_NEAR, SCALE, SWITCH_FAR
+	}
+	
+	public static enum OwnedSide {
+		UNKNOWN, LEFT, RIGHT
+	}
+	
+	public static OwnedSide getOwnedSide(GameFeature feature) {
+		String gsm = DriverStation.getInstance().getGameSpecificMessage();
+		if (gsm == null) {
+			return OwnedSide.UNKNOWN;	
+		}
+		if (gsm.length() < 3) {
+			return OwnedSide.UNKNOWN;
+		}
+		
+		int index = feature.ordinal();
+		
+		if (index >= 3 || index < 0) {
+			return OwnedSide.UNKNOWN;
+		}
+		
+		char gd = gsm.charAt(index);
+        switch (gd) {
+            case 'L':
+            case 'l':
+                return OwnedSide.LEFT;
+            case 'R':
+            case 'r':
+                return OwnedSide.RIGHT;
+            default:
+                return OwnedSide.UNKNOWN;
+        }
+	}
+}
