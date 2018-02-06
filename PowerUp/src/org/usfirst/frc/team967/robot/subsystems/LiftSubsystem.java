1109aaebad8b9159fa7d070975a07c81446309df
iterevo1 <iterevo@gmail.com>
2018-02-06 17:14:58 -0600

    removed stuff from last idea

--- PowerUp/src/org/usfirst/frc/team967/robot/subsystems/LiftSubsystem.java
+++ PowerUp/src/org/usfirst/frc/team967/robot/subsystems/LiftSubsystem.java
@@ -7,15 +7,13 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.PIDOutput;
-import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
-public class LiftSubsystem extends Subsystem implements PIDOutput {
+public class LiftSubsystem extends Subsystem {
 
 	private WPI_TalonSRX liftLead;
 	private WPI_TalonSRX liftFollow;
@@ -29,14 +27,6 @@ public class LiftSubsystem extends Subsystem implements PIDOutput {
 	private final double kD = RobotConstraints.LiftSubsystem_kD;
 	private final double kToleranceDegrees = RobotConstraints.LiftSubsystem_kTolerance;
 	
-	private PIDSource input;
-	public double PIDOutput;
-	
-	public int setPoint = 0;
-	
-	private int minimumSetPoint = 0;
-	private int maxSetPoint = 100;
-	
     public LiftSubsystem(){
     	liftLead = new WPI_TalonSRX(RobotMap.liftLead);
     	liftFollow = new WPI_TalonSRX(RobotMap.liftFollow);
@@ -45,65 +35,18 @@ public class LiftSubsystem extends Subsystem implements PIDOutput {
     	limitSwitchBottom = new DigitalInput(RobotMap.limitSwitchBottom);
     	
     	liftFollow.follow(liftLead);
-    	
-    	liftController = new PIDController(kP, kI, kD, input, liftLead);
-    	liftController.disable();
-    	liftController.setInputRange(0, 0);
-    	liftController.setOutputRange(-1, 1);
-    	liftController.setAbsoluteTolerance(kToleranceDegrees);
-    	liftController.setContinuous(false);
-    	
     }
     
     public void limit() {
     	boolean isTop = limitSwitchTop.get();
     	boolean isBottom = limitSwitchBottom.get();
-    	
-    	if (isTop) {
-    		setPoint = maxSetPoint; 
-    	}
-    	else if (isBottom) {
-    		setPoint = minimumSetPoint;
-    	}
     }
-    
-    public void changeSetpoint(int input) {
-    	setPoint = setPoint + input;
-    	pidSetSetpoint(setPoint);
-    } 
     
     public void moveLift(double power) {
     	liftLead.set(power);
 //    	liftFollow.set(power);
     }
     
-    
-    public void pidSetSetpoint(double input) {
-    	liftController.setSetpoint(input);
-    }
-    
-    public boolean isPidDone() {
-		if(liftController.getSetpoint() == 1) {
-			return true;
-		}
-		else {
-			return false;
-		}
-    }
-    
-    public void pidStart() {
-    	liftController.enable();
-    }
-    
-    public void pidStop() {
-    	liftController.disable();
-    }
-    
-	public void pidWrite(double output) {
-		PIDOutput = output;
-		
-	}
-	
 	public void initDefaultCommand() {}
     
     public void log() {}
