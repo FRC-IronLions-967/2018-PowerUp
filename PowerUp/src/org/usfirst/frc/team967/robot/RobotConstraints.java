54df62c8e08a0a7c5e680017c43d0e7c45ef4c69
iterevo1 <iterevo@gmail.com>
2018-02-10 09:04:15 -0600

    pid values

--- PowerUp/src/org/usfirst/frc/team967/robot/RobotConstraints.java
+++ PowerUp/src/org/usfirst/frc/team967/robot/RobotConstraints.java
@@ -3,9 +3,9 @@ package org.usfirst.frc.team967.robot;
 public class RobotConstraints {
 
 	public static final double DriveSubsystem_deadBand = .15;
-	public static final double DriveSubsystem_kP = 0.003; // 0.018
+	public static final double DriveSubsystem_kP = 0.0015; // 0.0015
 	public static final double DriveSubsystem_kI = 0;
-	public static final double DriveSubsystem_kD = 0.00128; // .02
+	public static final double DriveSubsystem_kD = 0.055; // .055
 	public static final double DriveSubsystem_kToleranceDegrees = 2.0f;
 	
 	public static final double LiftSubsystem_kP = 0;
