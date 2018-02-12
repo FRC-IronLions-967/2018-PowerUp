934c279b52de305228fc0c728e469cfacea9393b
iterevo1 <iterevo@gmail.com>
2018-02-11 19:21:17 -0600

    update for dashboard

--- customDashboardApp/testing/src/ui.js
+++ customDashboardApp/testing/src/ui.js
@@ -1,4 +1,4 @@
-/* DEFINE UI ELEMENTS BEFORE WRITING LISTENERS */
+/* DEFINE UI ELEMENTS BEFORE WRITING LISTENERS  */
 let ui = {
     timer: document.getElementById('timer'),
     robotState: document.getElementById('robot-state').firstChild,
